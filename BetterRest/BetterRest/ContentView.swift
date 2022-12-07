cb7d17227ec24ece9f442287ca1f737eefbd4b05
unhoLynx <darlynx13@gmail.com>
2022-12-07 15:42:15 +0300

    Improved UI

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,34 +9,75 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
+    /*
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
+     */
+    
+    var sleepResults: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            return "Your ideal bedtime is " + sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            return "There was an error"
+        }
+    }
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+            Form {
+                DatePicker("When do you want to wake up?",
+                               selection: $wakeUp,
+                               displayedComponents: .hourAndMinute)
                 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
+                Section {
+                    Stepper("\(sleepAmount.formatted()) hours",value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                }
                 
-                Text("Desired amount of sleep")
-                    .font(.headline)
+                Section {
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                } header: {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                }
                 
-                Stepper("\(sleepAmount.formatted()) hours",value: $sleepAmount, in: 4...12, step: 0.25)
+                Picker("Daily coffee intake",selection: $coffeeAmount) {
+                    ForEach(1...20, id: \.self) {
+                        $0 == 1 ? Text("\($0) cup") : Text("\($0) cups")
+                    }
+                }
                 
-                Text("Daily coffee intake")
-                    .font(.headline)
-                
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                Section {
+                    Text(sleepResults)
+                }
             }
             .navigationTitle("BetterRest")
+            /*
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
@@ -45,8 +86,10 @@ struct ContentView: View {
             } message: {
                 Text(alertMessage)
             }
+             */
         }
     }
+    /*
     func calculateBedtime() {
         do {
             let config = MLModelConfiguration()
@@ -68,6 +111,7 @@ struct ContentView: View {
         }
         showingAlert = true
     }
+    */
 }
 
 struct ContentView_Previews: PreviewProvider {
